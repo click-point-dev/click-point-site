4967f36b1ac8e4b53e5cefd13636e9920690def6
click-point <clickpoint2023@gmail.com>
2023-09-26 11:52:16 +0700

    add phone button

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,17 @@
 import { isMobile } from './functions.js';
 // Подключение списка активных модулей
 import { flsModules } from './modules.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+	const appearingElement = document.querySelector('[data-appearing]');
+
+	if (!appearingElement) return;
+
+	window.addEventListener('scroll', () => {
+		if (window.scrollY > 800) {
+			appearingElement.classList.add('_appearing');
+		} else {
+			appearingElement.classList.remove('_appearing');
+		}
+	});
+});
