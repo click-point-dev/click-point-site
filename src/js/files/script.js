128cf1dadff1a51381acb3856558f2b79820b67d
Roman Krets <romankrets@yandex.com>
2023-10-04 17:16:11 +0700

    about first-screen is done

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -26,20 +26,39 @@ CustomEase.create('astroEase', '.08,.32,0,1');
 gsap.from('.first-screen-about__main-image', {
 	y: '110%',
 	x: 200,
-	bottom: 1,
-	duration: 6,
+	duration: 5,
 	delay: 2.5,
 	ease: 'power4.out',
 	// ease: 'astroEase',
 });
+gsap.from('.first-screen-about__main-image', {
+	opacity: 0.5,
+	duration: 2,
+	delay: 3,
+	// ease: 'astroEase',
+});
+
 gsap.from('.first-screen-about__background', {
-	width: '180%',
-	objectPosition: 'center',
+	scale: 3,
+	transformOrigin: 'center top',
 	duration: 7,
 	delay: 1,
 	ease: 'power4.out',
 	// ease: 'astroEase',
 });
 
-gsap.from('.first-screen-about__decor', { x: '100%', y: '-100%', duration: 7, delay: 1, ease: 'power4.out' });
-gsap.from('.first-screen-about__decor', { opacity: 0, duration: 4, delay: 3, ease: 'power4.out' });
+gsap.from('.first-screen-about__decor-1', { right: -1000, y: '-100%', duration: 7, delay: 1, ease: 'power4.out' });
+gsap.from('.first-screen-about__decor-1', { opacity: 0, duration: 4, delay: 3, ease: 'power4.out' });
+
+gsap.from('.first-screen-about__decor-2', { y: 1500, duration: 7, delay: 1, ease: 'power4.out' });
+gsap.from('.first-screen-about__decor-2', { opacity: 0, duration: 8, delay: 2, ease: 'power4.out' });
+
+gsap.from('.first-screen-about__decor-3', {
+	top: '15%',
+	right: '30%',
+	width: 70,
+	duration: 7,
+	delay: 1,
+	ease: 'power4.out',
+});
+gsap.from('.first-screen-about__decor-3', { opacity: 0, duration: 7, delay: 3, ease: 'power4.out' });
