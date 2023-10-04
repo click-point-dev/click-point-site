acf9a57d2fbbba1dbacee4acd665ea208e9a2389
Roman Krets <romankrets@yandex.com>
2023-10-04 12:39:23 +0700

    start lerning gsap animations

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,7 +2,10 @@
 import { isMobile } from './functions.js';
 // Подключение списка активных модулей
 import { flsModules } from './modules.js';
+import gsap from 'gsap';
+import CustomEase from 'gsap/CustomEase.js';
 
+// добавление классов элементам при скроле на определенную величину
 window.addEventListener('DOMContentLoaded', () => {
 	const appearingElement = document.querySelector('[data-appearing]');
 
@@ -16,3 +19,27 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 });
+
+//+ gsap анимации
+CustomEase.create('astroEase', '.08,.32,0,1');
+
+gsap.from('.first-screen-about__main-image', {
+	y: '110%',
+	x: 200,
+	bottom: 1,
+	duration: 6,
+	delay: 2.5,
+	ease: 'power4.out',
+	// ease: 'astroEase',
+});
+gsap.from('.first-screen-about__background', {
+	width: '180%',
+	objectPosition: 'center',
+	duration: 7,
+	delay: 1,
+	ease: 'power4.out',
+	// ease: 'astroEase',
+});
+
+gsap.from('.first-screen-about__decor', { x: '100%', y: '-100%', duration: 7, delay: 1, ease: 'power4.out' });
+gsap.from('.first-screen-about__decor', { opacity: 0, duration: 4, delay: 3, ease: 'power4.out' });
