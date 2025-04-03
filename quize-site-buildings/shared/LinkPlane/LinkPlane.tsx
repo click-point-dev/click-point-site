6ae82aee9ae7f922e24ba2fc98c5e7dbb5bf98aa
Roman <romankrets@gmail.com>
2025-04-04 01:37:01 +0700

    implement LinkPlane

--- quize-site-buildings/shared/LinkPlane/LinkPlane.tsx
+++ quize-site-buildings/shared/LinkPlane/LinkPlane.tsx
@@ -3,6 +3,18 @@
 import Link from 'next/link';
 import { ReactNode } from 'react';
 
-export default function LinkPlane({ children, href }: { children?: ReactNode, href:string }) {
-	return <Link className='text-xs md:text-sm py-1.5 dark:hover:text-primary2 hover:text-primary1 transition' href={href}>{children}</Link>;
+export default function LinkPlane({
+	children,
+	href,
+}: {
+	children?: ReactNode;
+	href: string;
+}) {
+	return (
+		<Link
+			className='py-1.5 text-xs transition hover:text-primary1 md:text-sm dark:hover:text-primary2'
+			href={href}>
+			{children}
+		</Link>
+	);
 }
